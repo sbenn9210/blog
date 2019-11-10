65c176cc3c735647b7a7a0e25e25dc1d2e94fcfd
sbenn9210 <sbenn9210@gmail.com>
2019-11-09 21:16:38 -0600

    Add sign in and sign out actions

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -37,3 +37,14 @@ export const fetchUser = id => async dispatch => {
 
 //   dispatch({ type: "FETCH_USER", payload: response.data });
 // };
+
+export const signIn = () => {
+  return {
+    type: "SIGN_IN"
+  };
+};
+export const signOut = () => {
+  return {
+    type: "SIGN_OUT"
+  };
+};
