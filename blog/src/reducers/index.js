397488bad71b1486b1d614a7f4885ed093ea6425
sbenn9210 <sbenn9210@gmail.com>
2019-11-14 22:48:35 -0600

    Working version of redux forms

--- blog/src/reducers/index.js
+++ blog/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import postReducer from "./postsReducer";
 import usersReducer from "./usersReducer";
 import authReducer from "./authReducer";
+import { reducer as formReducer } from "redux-form";
 
 export default combineReducers({
   posts: postReducer,
   users: usersReducer,
-  auth: authReducer
+  auth: authReducer,
+  form: formReducer
 });
