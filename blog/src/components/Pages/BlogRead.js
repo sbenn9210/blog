fa76312a61d0b533866ba47ca25131c736fd06be
sbenn9210 <sbenn9210@gmail.com>
2019-12-15 19:49:58 -0600

    Adding edit mode and read mode"

--- blog/src/components/Pages/BlogRead.js
+++ blog/src/components/Pages/BlogRead.js
@@ -2,11 +2,16 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class BlogRead extends Component {
+  handleEdit = () => {
+    this.props.history.push("/blogs/edit");
+  };
   render() {
-    console.log(this);
     const { post } = this.props;
     return (
       <div>
+        <button onClick={this.handleEdit}>
+          <i class="edit icon"></i>
+        </button>
         <h1>{post.title}</h1>
         <p>{post.body}</p>
       </div>
