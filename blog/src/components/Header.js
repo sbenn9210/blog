06d58ae74bfb0ecd3a193347169b444566c51bc3
sbenn9210 <sbenn9210@gmail.com>
2020-01-12 18:34:36 -0600

    Align navigate menu items

--- blog/src/components/Header.js
+++ blog/src/components/Header.js
@@ -1,23 +1,41 @@
 import React, { Component } from "react";
-import glamorous from "glamorous";
 import GoogleAuth from "./GoogleAuth";
 class Header extends Component {
   render() {
+    const headerStyle = {
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center",
+      marginBottom: "25px",
+      maxWidth: "1033px",
+      marginLeft: "auto",
+      marginRight: "auto",
+      height: "60px"
+    };
+
+    const rightIcons = {
+      display: "flex",
+      justifyContent: "flex-end",
+      alignItems: "center"
+    };
+
+    const buttonSpacing = {
+      marginRight: "24px"
+    };
     return (
-      <Div>
+      <div style={headerStyle}>
         <h1>Blogger</h1>
-        <h3>Become a member</h3>
-        <h3>Get Started</h3>
-        <GoogleAuth />
-      </Div>
+        <div style={rightIcons}>
+          <i style={buttonSpacing} className="large search icon" />
+          <i style={buttonSpacing} className="large bell outline icon" />
+          <button style={buttonSpacing} className="ui black basic button">
+            Upgrade
+          </button>
+          <GoogleAuth />
+        </div>
+      </div>
     );
   }
 }
 
 export default Header;
-
-const Div = glamorous.div({
-  display: "flex",
-  justifyContent: "space-evenly",
-  marginBottom: "25px"
-});
